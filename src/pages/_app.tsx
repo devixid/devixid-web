ba540e7c3f928d9e89087b47530c6cbe88ca1db7
andrianfaa <hi@anfa.my.id>
2023-03-22 23:54:07 +0700

    feat: add title to all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { DefaultSeo } from "next-seo";
 import type { AppProps } from "next/app";
 
 import { NavigationBar } from "@/components";
@@ -7,8 +8,17 @@ import "@/styles/globals.css";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <NavigationBar />
+      <DefaultSeo
+        title="Devix.id - your best website development partner"
+        description="Devix.id is a team of young website developers based in Indonesia since 2020. We are ready to work with you to build your dream website. Contact us for more information and feel free to talk to us about your ideas."
+        openGraph={{
+          title: "Devix.id - your best website development partner",
+          description:
+            "Devix.id is a team of young website developers based in Indonesia since 2020. We are ready to work with you to build your dream website. Contact us for more information and feel free to talk to us about your ideas."
+        }}
+      />
 
+      <NavigationBar />
       <Component {...pageProps} />
     </>
   );
