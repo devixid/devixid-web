a79b1e39f646a4952bf5df4b277b3a1c273e8019
andrianfaa <hi@anfa.my.id>
2023-03-23 02:06:28 +0700

    chore: update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ docker build -t devixid-image .
 if getopts "d" args;
   then
     # run docker in detach mode
-    docker run -d -p 8080:3000 devixid-image
+    docker run --name=devixid-container -d -p 8080:3000 devixid-image
   else
     # run docker in normal mode
-    docker run -p 8080:3000 devixid-image
+    docker run --name=devixid-container -p 8080:3000 devixid-image
 fi
